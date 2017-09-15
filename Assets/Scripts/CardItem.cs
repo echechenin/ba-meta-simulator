b5093755ece304b998ee3d465c79958a3c27545c
Alexandrovskiy Albert <albert@pixonic.com>
2017-09-15 15:54:37 +0300

    merge project, add gitignor

---
+++ Assets/Scripts/CardItem.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CardItem : MonoBehaviour {
+
+	private Image cardImage;
+	private Text cardText;
+
+	// Use this for initialization
+	void Start () {
+		cardImage = GetComponent<Image> ();
+		cardText = GetComponentsInChildren<Text> ()[0];
+	}
+	
+	public void SetCardItem(Item item, int cardCount)
+	{
+		cardImage = GetComponent<Image> ();
+		cardText = GetComponentsInChildren<Text> ()[0];
+		string iconPath = "UI/ItemsImg/" + item.name;
+		Debug.Log (iconPath);
+		cardImage.sprite = Resources.Load<Sprite>("UI/ItemsImg/"+item.name);
+		cardText.text = "x"+cardCount.ToString ();
+	}
+}
---
+++ Assets/Scripts/ChestsController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChestsController : MonoBehaviour {
+	public GameObject chestRewardPopup;
+	public GameObject itemCardPrefab;
+	public int itemCount;
+	public string[] itemArr;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void OpenChest()
+	{
+		chestRewardPopup.SetActive (true);
+		GameObject content = GameObject.Find ("Content");
+		foreach (Transform child in content.transform)
+		{
+				Destroy (child.gameObject);
+		}
+		for(int i = 0; i < itemCount; i++)
+		{
+			GameObject itemCardButton = Instantiate (itemCardPrefab, content.transform);
+			CardItem cardItem = itemCardButton.GetComponent<CardItem> ();
+			Debug.Log (cardItem.name);
+			cardItem.SetCardItem (new Item (itemArr[Random.Range(0,9)]), Random.Range(1,13));
+		}
+	}
+}
