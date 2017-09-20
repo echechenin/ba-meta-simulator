bf083ed376e1062bc3c5f096604e7a32d3b572e1
M799\e.chechenin <e.chechenin@pixonic.ru>
2017-09-20 19:07:10 +0300

    017. battlereward (in progress)

---
+++ Assets/Scripts/BattleRewardView.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleRewardView : MonoBehaviour {
+	public Text battleResult;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
