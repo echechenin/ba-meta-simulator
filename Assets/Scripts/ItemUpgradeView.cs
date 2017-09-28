09aec7b683317777820f64e7e4d3326f53ed60d9
Alexandrovskiy Albert <albert@pixonic.com>
2017-09-28 18:10:04 +0300

    add item upgrade window

--- Assets/Scripts/ItemUpgradeView.cs
+++ Assets/Scripts/ItemUpgradeView.cs
@@ -7,6 +7,7 @@ public class ItemUpgradeView : MonoBehaviour {
 
 	public InventoryController inventoryController;
 	public Image itemImage;
+	public Image itemImageSmall;
 
 	public Text dialogTitle; 
 	public Text itemNameTitle;
@@ -26,23 +27,24 @@ public class ItemUpgradeView : MonoBehaviour {
 	{
 		itemNameTitle.text = item.name;
 		itemImage.sprite = Resources.Load<Sprite> ("UI/ItemsImg/" + item.name);
-		dialogTitle.text = "Улучшить" + item.name + " до " + (item.level + 1).ToString () + " уровня";
+		itemImageSmall.sprite = Resources.Load<Sprite> ("UI/ItemsImg/" + item.name);
+		dialogTitle.text = "Улучшить <b>" + item.name + "</b> до <b>" + (item.level + 1).ToString () + "</b> уровня";
 		itemLevel.text = "Текущий уровень: " + item.level.ToString();
 		itemRequiredSourceItems.text = item.itemDefinition.partsForUpgrade [item.level] + " / " + inventoryController.GetSourceItemCount (item.name);
 
 		healthTitle.text = item.itemDefinition.bonusHealth [item.level - 1].ToString();
 		healthBonusTitle.text = "+" + (item.itemDefinition.bonusHealth [item.level] - item.itemDefinition.bonusHealth [item.level - 1]).ToString();
 
-		strengthTitle.text = item.itemDefinition.bonusHealth [item.level - 1].ToString();
-		strengthBonusTitle.text = "+" + (item.itemDefinition.bonusHealth [item.level] - item.itemDefinition.bonusHealth [item.level - 1]).ToString();
+		strengthTitle.text = item.itemDefinition.bonusStrength [item.level - 1].ToString();
+		strengthBonusTitle.text = "+" + (item.itemDefinition.bonusStrength [item.level] - item.itemDefinition.bonusStrength [item.level - 1]).ToString();
 
-		defenseTitle.text = item.itemDefinition.bonusHealth [item.level - 1].ToString();
-		defenseBonusTitle.text = "+" + (item.itemDefinition.bonusHealth [item.level] - item.itemDefinition.bonusHealth [item.level - 1]).ToString();
+		defenseTitle.text = item.itemDefinition.bonusDefense [item.level - 1].ToString();
+		defenseBonusTitle.text = "+" + (item.itemDefinition.bonusDefense [item.level] - item.itemDefinition.bonusDefense [item.level - 1]).ToString();
 
-		penetrationTitle.text = item.itemDefinition.bonusHealth [item.level - 1].ToString();
-		penetrationBonusTitle.text = "+" + (item.itemDefinition.bonusHealth [item.level] - item.itemDefinition.bonusHealth [item.level - 1]).ToString();
+		penetrationTitle.text = item.itemDefinition.bonusPenetration [item.level - 1].ToString();
+		penetrationBonusTitle.text = "+" + (item.itemDefinition.bonusPenetration [item.level] - item.itemDefinition.bonusPenetration [item.level - 1]).ToString();
 
-
+		buttonUpgrade.text = "Улучшить " + item.itemDefinition.softForUpgrade [item.level].ToString();
 	}
 
 	public void UpgradeCurrentItem()
